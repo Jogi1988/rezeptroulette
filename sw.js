e4a97ef17c2032caaa62215e5cf41679a8a75e53
Jochen.Weiland <jochen.jw.weiland@bmw.de>
2026-05-07 21:54:10 +0200

    feat: Service Worker für PWA-Installierbarkeit (Chrome Add to Homescreen)

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Minimal Service Worker für PWA-Installierbarkeit
+const CACHE_NAME = 'rezeptroulette-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll(['/', '/index.html', '/icon.svg', '/icon-192.png', '/icon-512.png'])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
